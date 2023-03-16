3f853da65f5e0b59386af00badd121417c0e6e64
Nurlan <Nazarov.Nurlan@gmail.com>
2023-03-16 22:21:31 +0400

    Added Product & Category controller(manage)

---
+++ Back-End-Project/Areas/Manage/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Back_End_Project.DataAccessLayer;
+using Back_End_Project.Models;
+using Back_End_Project.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_End_Project.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CategoryController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> Index(int pageIndex = 1)
+        {
+            IQueryable<Category> categories = _context.Categories.Include(c => c.Products)
+                .Where(c => c.IsDeleted == false );
+            return View(PageNatedList<Category>.Create(categories, pageIndex, 3));
+        }
+    }
+}
---
+++ Back-End-Project/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Back_End_Project.DataAccessLayer;
+using Back_End_Project.Models;
+using Back_End_Project.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back_End_Project.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(AppDbContext context, IWebHostEnvironment webHostEnvironment = null)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index(int pageIndex = 1)
+        {
+            IQueryable<Product?> products = _context.Products.Where(p => p.IsDeleted == false);
+
+            return View(PageNatedList<Product>.Create(products, pageIndex, 3));
+        }
+    }
+}
