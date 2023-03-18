46d2b696961aebd33112176dd3897ee1eafba3af
Nurlan-N <Nurlanbn@code.edu.az>
2023-03-18 17:20:47 +0400

    Added Setting CRUD

--- Back-End-Project/Areas/Manage/Controllers/SettingController.cs
+++ Back-End-Project/Areas/Manage/Controllers/SettingController.cs
@@ -1,7 +1,9 @@
 ﻿using Back_End_Project.DataAccessLayer;
+using Back_End_Project.Helpers;
 using Back_End_Project.Models;
 using Back_End_Project.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Back_End_Project.Areas.Manage.Controllers
 {
@@ -23,5 +25,41 @@ namespace Back_End_Project.Areas.Manage.Controllers
 
             return View(PageNatedList<Setting>.Create(settings, pageIndex, 3));
         }
+        [HttpGet]
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null) { return BadRequest(); }
+            Setting setting = await _context.Settings.FirstOrDefaultAsync(c => c.Id == id );
+
+            if (setting == null) { return NotFound(); }
+
+            ViewBag.Settings = await _context.Settings.ToListAsync();
+
+
+            return View(setting);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id, Setting setting)
+        {
+            ViewBag.Settings = await _context.Settings.ToListAsync();
+
+            if (!ModelState.IsValid)
+            {
+                return View(setting);
+            }
+            if (id == null) { return BadRequest(); }
+            if (id != setting.Id) { return BadRequest(); }
+
+            Setting settingDb = await _context.Settings.FirstOrDefaultAsync( s => s.Id == id);
+            if (settingDb == null) { return NotFound(); }
+
+
+            settingDb.Value = setting.Value.Trim();
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+       
     }
 }
