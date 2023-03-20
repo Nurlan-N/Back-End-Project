3b739749e181a931ea3da05137ea5363b9fd50f4
Nurlan-N <Nurlanbn@code.edu.az>
2023-03-20 23:10:54 +0400

    Added Contact View

--- Back-End-Project/Controllers/BlogController.cs
+++ Back-End-Project/Controllers/BlogController.cs
@@ -2,6 +2,7 @@
 using Back_End_Project.Models;
 using Back_End_Project.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Back_End_Project.Controllers
 {
@@ -19,6 +20,17 @@ namespace Back_End_Project.Controllers
             IQueryable<Blog?> blogs = _context.Blogs.Where(b => b.IsDeleted == false);
 
             return View(PageNatedList<Blog>.Create(blogs, pageIndex, 6));
+        }
+        public async Task<IActionResult> Detail(int id)
+        {
+            ViewBag.Blog = await _context.Blogs.Where(b => b.IsDeleted == false).ToListAsync(); 
+            if (id == null) { return BadRequest(); }
+            Blog blog = await _context.Blogs
+                .FirstOrDefaultAsync(p => p.IsDeleted == false && p.Id == id);
+
+            if (blog == null) { return NotFound(); }
+
+            return View(blog);
         }
     }
 }
