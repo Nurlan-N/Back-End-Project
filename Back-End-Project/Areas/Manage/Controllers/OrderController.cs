07e007ae2a4f8ea146c675bbcdaf3679ee145955
Nurlan-N <Nurlanbn@code.edu.az>
2023-03-26 00:05:33 +0400

    Added OrderController (manage)

---
+++ Back-End-Project/Areas/Manage/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Back_End_Project.DataAccessLayer;
+using Back_End_Project.Models;
+using Back_End_Project.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_End_Project.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    [Authorize(Roles ="SuperAdmin,Admin")]
+    public class OrderController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public OrderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int pageIndex = 1)
+        {
+            IQueryable<Order> orders = _context.Orders
+                .Include(o => o.OrderItems);
+
+            return View(PageNatedList<Order>.Create(orders, pageIndex, 5));
+        }
+    }
+}
